dbbbca3ecd385af1961dfb6c5ed3ac93e6dd2303
hejny <me@pavolhejny.com>
2018-02-11 22:36:16 +0100

    something

--- src/index.js
+++ src/index.js
@@ -16,16 +16,48 @@ setInterval(()=>{
 
 
 
-
 const SceneComponent = observer(class extends React.Component {
     render() {
         return (
             <Scene>
-                <Entity geometry={{primitive: 'box'}} material={{color: this.props.data.color}} position={{x: 0, y: 0, z: -5}}/>
-                <Entity particle-system={{preset: 'snow'}}/>
+
+                {/*<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" shadow></a-box>
+                <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" shadow></a-sphere>
+                <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D" shadow></a-cylinder>
+                <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+                <a-sky color="#ECECEC"></a-sky>*/}
+
+
+
+                <Entity
+                    geometry={{primitive: 'box'}}
+                    material={{color: this.props.data.color}}
+                    position={{x: 0, y: 4, z: -5}}
+
+                    events={{
+                        click: (event) => {
+                            console.log('Clicked!',event);
+                        },
+                        collided: [(event) => {
+                            console.log('Collided!',event);
+                        }]}}
+
+
+                />
+                <Entity geometry={{primitive: 'sphere'}} material={{color: this.props.data.color}} position={{x: 0, y: 0, z: -5}}/>
+                <Entity primitive='a-sky' material={{color: '#85e9ec'}}/>
+
+
+
+               {/* <a-entity hand-controls="left" aabb-collider="objects: .cube;" grab></a-entity>
+                <a-entity hand-controls="right" aabb-collider="objects: .cube;" grab></a-entity>*/}
+
+
+
+                {/*<Entity particle-system={{preset: 'snow'}}/>
                 <Entity light={{type: 'point'}}/>
                 <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-                <Entity text={{value: 'Hello, WebVR!'}}/>
+                <Entity text={{value: 'Hello, WebVR!'}}/>*/}
             </Scene>
         );
     }
